aff8b25138e98e59233e2357de4bb7722e40b07b
svnlabs <svnlabs@gmail.com>
2017-08-22 11:27:40 +0530

    php extensions

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php
-    $body = '';
-    while (FALSE !== ($line = fgets(STDIN))) {
-	$body.= $line;
-    }
-
-    $event = json_decode($body,true);
-    var_dump($event);
-
-    print_r(get_loaded_extensions());
+    
+ print_r(get_loaded_extensions());
 
 ?>
