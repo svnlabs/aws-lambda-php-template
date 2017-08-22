d27b5a528a75120bac5d19ec2bbde90135c0615f
svnlabs <svnlabs@gmail.com>
2017-08-22 11:25:52 +0530

    Exec PHP Version
    Exec PHP Version

--- index.js
+++ index.js
@@ -1,37 +1,19 @@
-// For development/testing purposes
+'use strict';
+exports.handler = function(event, context, callback) {
+  var exec = require('child_process').exec;
 
-var spawn = require('child_process').spawn
-var stream = require('stream')
-
-exports.handler = function( event, context ) {
-
-	var php =spawn('./php', ['index.php'])
-	php.stdin.setEncoding = 'utf-8';
-	php.stdin.write(JSON.stringify(event) + "\n")
-	php.stdin.end()
-
-	//php.stdout.pipe(process.stdout);
-	
-	
-
-	php.stdout.on('data', function (data) {
-		console.log( data.toString() );
-	});
-
-	php.stderr.on('data', function (data) {
-		console.log( data.toString() );
-	});
-
-//var readable = new stream.Readable();
-//readable._read = function noop() {}; // See note below
-//readable.push('test me!');
-//readable.push(null);
-//readable.pipe(php.stdin)
-
-	php.on('exit', function (code) {
-  		console.log('child process exited with code ' + code);
-		context.done()
-	});
-
-}
+  exec('./php -v', function (error, stdout, stderr) {
+    if(stdout){
+      console.log('stdout: ' + stdout);
+    }
+    if(stderr){
+      console.log('stderr: ' + stderr);
+    }
+    if (error !== null) {
+      console.log('Exec error: ' + error);
+    }
+    context.succeed(stdout);
+    console.log(stdout);
+  });
+};
 
